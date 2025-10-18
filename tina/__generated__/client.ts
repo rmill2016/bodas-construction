8e6b1e0f55f92077da94bf150b21e2a9eab83f48
Ryan Mill <rmill2016@gmail.com>
2025-10-17 17:52:28 -0700

    update to homepage css

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '5750fae78c7b32bd30f80bbfaac31b1a2f921181', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
 export default client;
   
